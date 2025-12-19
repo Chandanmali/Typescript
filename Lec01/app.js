b0c0563f44989baeb17890c9102cfa623052745d
Chandan Mali <chandanmali21117@gmail.com>
2025-12-19 10:44:08 +0530

    basic types done

--- Lec01/app.js
+++ Lec01/app.js
@@ -1,7 +1,14 @@
-function add(num1, num2) {
-    return (num1 + num2);
+function add(num1, num2, res, statement) {
+    if (res === true) {
+        return "".concat(statement, " : ").concat(num1 + num2);
+    }
+    else {
+        return 0;
+    }
 }
 var num1 = 10;
-var num2 = 200;
-var ans = add(num1, num2);
+var num2 = 100;
+var result = true;
+var statement = "The ans is ";
+var ans = add(num1, num2, result, statement);
 console.log(ans);
--- Lec01/app.ts
+++ Lec01/app.ts
@@ -1,9 +1,18 @@
-function add(num1, num2){
-    return (num1+num2)
+function add(num1 : number, num2 : number, res : boolean, statement : string){
+    if(res === true)
+    {
+        return `${statement} : ${num1+num2}` 
+    }
+    else{
+        return 0
+    }
 }
 
 const num1 = 10;
-const num2 = 200;
+const num2 = 100;
+const result = true
+const statement = "The ans is "
 
-const ans = add(num1, num2)
+
+const ans = add(num1, num2, result, statement)
 console.log(ans)
